314168bca3cf189dab0cbc6bd07f0af1c60d04e3
Salman Ahmad Bhatti <salmanbhatti52@hotmail.com>
2022-11-16 15:14:23 +0500

    ios update
    ios update

--- android/app/src/main/kotlin/com/knaw/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/knaw/app/MainActivity.kt
@@ -1,21 +1,6 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> b333b914e99a5e1d8febc5cf160a8d126d3b80da
 package com.knaw.app
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
-<<<<<<< HEAD
-=======
-=======
-package com.knaw.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
->>>>>>> 703bfd9dc938819c072141626024714eff58f344
->>>>>>> b333b914e99a5e1d8febc5cf160a8d126d3b80da
